ff8903319213e1b619019ab29ef32affc3e76563
Al Wirtes <wirtes@gmail.com>
2022-08-30 15:55:05 -0600

    Now respecting file name sort order. Useful for maintaining sequence of photos.

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import os, sys
 
 
 # Configs
-local_directory = "1975/" # Add edning slash
-slug = "1975 Las Vegas April, Barb St. Marys Fall, Thanksgiving and Christmas at Miller School, "
+local_directory = "1983/" # Add ending slash
+slug = "1983 Summer, Brian Dolan baby photo shoot, hawk in back yard, "
 extension = ".jpg"
 
-picture_directory = "/Volumes/USB/DCIM/100MEDIA/" # Add ending slash
+picture_directory = "/Users/wirtes/OneDrive/Pictures/Archive Photo Scans/_Processing/" # Add ending slash
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     working_directory = picture_directory + local_directory
     dir_list = os.listdir(working_directory)
+    dir_list.sort()
     os.chdir(working_directory)
     print("Files and directories in '", working_directory, "' :")
     file_count = 1
